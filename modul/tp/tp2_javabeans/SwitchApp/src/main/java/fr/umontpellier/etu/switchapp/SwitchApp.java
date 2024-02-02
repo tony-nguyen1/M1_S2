1ab13561c754b7b048363d87c99901102a7685e2
tony-nguyen1 <nguyentoony@gmail.com>
2024-02-02 11:09:10 +0100

    modularite tp javabeans

---
+++ modul/tp/tp2_javabeans/SwitchApp/src/main/java/fr/umontpellier/etu/switchapp/SwitchApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.umontpellier.etu.switchapp;
+
+/**
+ *
+ * @author tony.nguyen@etu.umontpellier.fr
+ */
+public class SwitchApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
