4822d27535b89d4071dfa52139098f8e2fc45b91
tony-nguyen1 <nguyentoony@gmail.com>
2024-02-08 11:06:38 +0100

    avance minuscule

--- web/Sf6GestionTournoi/README.md
+++ web/Sf6GestionTournoi/README.md
@@ -1,3 +1,8 @@
 
 symfony create-project symfony/skeleton
 symfony server:start
+symfony console doctrine:query:sql "create table test(nom varchar(10));"
+symfony console doctrine:query:sql "insert into test values ('dupont');"
+symfony composer req maker --dev
+symfony console make:entity
+//Next: When you're ready, create a migration with symfony console make:migration
---
+++ web/Sf6GestionTournoi/src/Entity/Evenement.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EvenementRepository::class)]
+class Evenement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateDeb = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateFin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateDeb(): ?\DateTimeInterface
+    {
+        return $this->dateDeb;
+    }
+
+    public function setDateDeb(\DateTimeInterface $dateDeb): static
+    {
+        $this->dateDeb = $dateDeb;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $dateFin): static
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+}
